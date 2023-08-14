fd981e44920884e5fa1e6e43b36e85436425009a
shaninalex <shanin.x@gmail.com>
2023-08-14 13:43:39 +0300

    lu-button styles

--- projects/lunaria/src/lib/components/lu-button/lu-button.component.ts
+++ projects/lunaria/src/lib/components/lu-button/lu-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'button[lu-button],input[lu-button],div[lu-button],a[lu-button]',
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class LuButtonComponent {
 
+    @Input("appearance") appearance: "solid" | "outline" = "solid";
+    @Input("theme") theme: "rounded" | "pilled" = "rounded";
+    @Input("color") color: "regular" | "warning" | "success" | "secondary" = "regular" 
+
+    @HostBinding("class")
+    get classes(): string {
+        return `
+        ${this.__rounded_classes()}
+        ${this.__appearance_classes()}
+        ${this.__appearance_colors()}
+        `
+    }
+
+    private __appearance_classes(): string {
+        return this.appearance === "solid"? "": "lu-btn-outline"
+    }
+
+    private __rounded_classes(): string {
+        let css_cls: string = "regular";
+        switch (this.theme) {
+            case "rounded":
+                css_cls = "";
+                break;
+            case "pilled":
+                css_cls = "lu-btn-pilled"
+                break;
+        }
+        return css_cls;
+    }
+
+    private __appearance_colors(): string {
+        let css_cls: string = "regular";
+        switch (this.color) {
+            case "regular":
+                css_cls = "lu-btn-regular";
+                break;
+            case "warning":
+                css_cls = "lu-btn-warning"
+                break;
+            case "success":
+                css_cls = "lu-btn-success"
+                break;
+            case "secondary":
+                css_cls = "lu-btn-secondary"
+                break;
+        }
+
+        return css_cls;
+    }
 }
