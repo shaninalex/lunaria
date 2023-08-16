88fe3c87ec4277e3c3dbc98d13d1ce19b120c0eb
shaninalex <shanin.x@gmail.com>
2023-08-16 08:51:39 +0300

    add input

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AlertModule } from '../../../lunaria/src/lib/components/alert/alert.module';
 import { LuButtonModule } from 'projects/lunaria/src/lib/components/lu-button/lu-button.module';
-
+import { LuInputModule } from 'projects/lunaria/src/lib/components/lu-input/lu-input.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -15,6 +15,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     AlertModule,
     LuButtonModule,
+    LuInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/lunaria/src/lib/components/lu-input/lu-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'lu-input',
+    templateUrl: './lu-input.component.html',
+    styleUrls: ['./lu-input.component.scss']
+})
+export class LuInputComponent {
+    @Input('icon') icon: string | null;
+
+    constructor() {
+        this.icon = null
+    }
+
+}
