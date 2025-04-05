68feecf7ae042d51f8e65a93fe2026b227f2be83
shaninalex <shanin.x@gmail.com>
2025-04-05 10:48:45 +0300

    feat: basic setup

--- projects/lunaria/src/public-api.ts
+++ projects/lunaria/src/public-api.ts
@@ -1,8 +1,9 @@
 /*
  * Public API Surface of lunaria
  */
-export * from './lib/components';
 
-// export * from './lib/lunaria.service';
-// export * from './lib/lunaria.component';
-// export * from './lib/lunaria.module';
+export * from './lib/lunaria.service';
+export * from './lib/lunaria.component';
+export * from './lib/lunaria.module';
+
+export * from './lib/button';
